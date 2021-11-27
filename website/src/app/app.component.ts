6db02e9f2bceeb3a64f5f798ab04f353c5dc37b7
Lars Willrich <larswillrich@gmail.com>
2021-11-27 20:21:28 +0000

    add old stuff

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fewo-willrich';
+
+  constructor(private httpClient: HttpClient) { 
+	  console.log("alskdjasdjlkasdjlka");
+	  this.sendGetRequest();
+  }
+
+  public sendGetRequest(){
+
+  	//const headers = new HttpHeaders().set('Content-Type', 
+
+    this.httpClient.get('https://udx62k4em2.execute-api.eu-central-1.amazonaws.com/prod/newEntry').subscribe(val => console.log(val));
+  }
+
+  //events1 = this.HttpClient.get('https://halle-online-kalender.s3.eu-central-1.amazonaws.com/db.json');
+
+//console.log(alskdjasdjlkasdjlka");
+
+
+  events = {
+	"events": ["Hello World","Hello World1","Hello World2"]
+	}
 }
